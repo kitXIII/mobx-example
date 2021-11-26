536fd2d3c73c9403c5085cc1dba6a7d07745bf14
Nikita Konovalov <n.konovalov@galament.com>
2021-11-26 12:56:38 +0400

    auto-observable

--- src/stores/TodoStore.ts
+++ src/stores/TodoStore.ts
@@ -1,4 +1,4 @@
-import { action, makeObservable, observable } from "mobx";
+import { makeAutoObservable } from "mobx";
 
 export interface Todo {
     id: number;
@@ -10,11 +10,9 @@ class TodoStore {
     list: Todo[] = [];
 
     constructor() {
-        makeObservable(this, {
-            list: observable,
-            add: action,
-            toggle: action,
-            remove: action
+        makeAutoObservable(this, {
+            // add: false,
+            // add: computed,
         });
     }
 

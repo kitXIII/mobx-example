b1dfa4b66745417fbf2492bf4af24397eb35c1fb
Nikita Konovalov <n.konovalov@galament.com>
2021-11-26 13:34:21 +0400

    hack observable and useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,32 @@
-import React from 'react';
+import { useState } from 'react';
+import { observable } from 'mobx';
 import TodoInput from './Todo/TodoInput';
 import TodoList from './Todo/TodoList';
+import styles from './App.module.css';
+import { observer } from 'mobx-react-lite';
 
 const App = () => {
+    const [appUI] = useState(() =>
+        observable({
+            todosVisible: true,
+            toggleTodosVisibility() {
+                this.todosVisible = !this.todosVisible;
+            },
+        }),
+    );
+
     return (
         <div className='app'>
             <TodoInput />
-            <TodoList />
+            <div className={styles['todo-list-wrapper']}>
+                <h2 onClick={() => appUI.toggleTodosVisibility()}>
+                    <span>{appUI.todosVisible ? '-' : '+'}</span>
+                    Todos
+                </h2>
+                {appUI.todosVisible ? <TodoList /> : null}
+            </div>
         </div>
     );
 };
 
-export default App;
+export default observer(App);
